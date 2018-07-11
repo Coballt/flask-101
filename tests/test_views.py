554bb84cede61caeed3746d75616febc1e34ed84
Thibaut DELAROZIERE <thibaut.delaroziere@gmail.com>
2018-07-11 11:03:17 +0200

    Adding path for unit product GET

--- tests/test_views.py
+++ tests/test_views.py
@@ -12,3 +12,16 @@ class TestViews(TestCase):
         products = response.json
         self.assertIsInstance(products, list)
         self.assertGreater(len(products), 3) # 3 is not a mistake here.
+
+    def test_product_unit_works(self):
+        response = self.client.get("/api/v1/products/2")
+        product = response.json
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['name'], 'Socialive.tv')
+        self.assertEqual(response.json['id'], 2)
+
+    def test_product_unit_not_found(self):
+        response = self.client.get("/api/v1/products/89087654")
+        product = response.json
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json['error'], 'Product not found')
