965745480a59abeb1efbcc7bd95e68a4284ae244
Thibaut DELAROZIERE <thibaut.delaroziere@gmail.com>
2018-07-11 10:36:35 +0200

    API product v1

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,16 @@
 # wsgi.py
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/api/v1/products')
+def products():
+    the_products = [
+        { 'id': 1, 'name': 'Skello' },
+        { 'id': 2, 'name': 'Socialive.tv' }
+    ]
+    return jsonify(the_products)
